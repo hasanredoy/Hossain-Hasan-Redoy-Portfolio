2ed05ad0810c455c802dd0c1a734f16def4ff82f
unknown <redoyhasan4585@gmail.com>
2024-06-27 11:39:31 +0400

    first commit working on navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+       {/* logo  */}
+      <h1>H H {"< redo `/ />"} </h1>
+    </div>
+  );
+};
+
+export default Navbar;
