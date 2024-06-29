38a26dbf6f3965cca90156e2ca163ce1c98d4993
unknown <redoyhasan4585@gmail.com>
2024-06-30 00:12:40 +0400

    added project section

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -4,6 +4,9 @@ import useAxios from "@/Hooks/useAxios";
 import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
+import { FaArrowUpRightFromSquare } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+import Link from "next/link";
 
 const axiosHook = useAxios();
 const Projects = () => {
@@ -22,7 +25,7 @@ const Projects = () => {
     <div className=" my-20 container mx-auto">
       {/* text div  */}
       <div className=" mb-10">
-        <h1 className=" text-5xl font-bold w-80 text-center border-b-2 shadow-xl shadow-stone-500 mx-auto">
+        <h1 className=" text-3xl lg:text-5xl font-bold w-80 text-center border-b-2 shadow-xl shadow-stone-500 mx-auto">
           My Projects
         </h1>
       </div>
@@ -41,8 +44,8 @@ const Projects = () => {
               </div>
               {/* text div */}
               <div className=" flex-1">
-                <h1 className=" text-2xl font-bold">{project?.title}</h1>
-                <p className=" my-5 text-lg">{project?.description}</p>
+                <h1 className=" text-xl md:text-2xl font-bold">{project?.title}</h1>
+                <p className=" my-5 text-base md:text-lg">{project?.description}</p>
                 {/* feature div  */}
                 <div className=" my-5">
                   <h3 className=" text-xl font-bold">Features</h3>
@@ -50,16 +53,32 @@ const Projects = () => {
                     {feature}
                   </li>)}</ul>
                 </div>
-              {/* skills div  */}
-                <div className=" grid grid-cols-4  gap-2">
+              {/* technologies div  */}
+              <h3 className=" text-xl font-bold mb-5">Techs I used</h3>
+                <div className=" grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-2">
                   {project?.techs?.map(tech=>{
                     return <div key={tech}>
-                      <button className=" bg-stone-900 text-white font-bold  py-2 px-5 rounded-md w-full ">{tech}</button>
+                      <button className=" bg-stone-900 text-white text-sm lg:text-base font-bold  py-2 px-5 rounded-md w-full ">{tech}</button>
                     </div>
                   })}
                 </div>
 
                 {/* links div  */}
+                <hr className=" my-5" />
+                <div>
+                  <h3 className=" text-xl font-bold my-5"> Please Visit </h3>
+                  <div className=" grid grid-cols-2 md:grid-cols-3 gap-5 lg:gap-10 ">
+                  <Link href={project?.live} target="_blank">
+                  <button className=" flex gap-5 items-center bg-green-700 text-white font-bold rounded-lg px-3 py-2">Live Site <FaArrowUpRightFromSquare></FaArrowUpRightFromSquare></button></Link>
+                  <Link href={project?.clientRepo} target="_blank">
+                  <button className=" flex gap-5 items-center bg-gray-900 text-white font-bold rounded-lg px-3 py-2"><FaGithub className=" text-2xl"></FaGithub> Client</button>
+                  </Link>
+                  <Link href={project?.serverRepo?project?.serverRepo:project?.clientRepo} target="_blank">
+                  <button className=" flex gap-5 items-center bg-gray-900 text-white font-bold rounded-lg px-3 py-2"><FaGithub className=" text-2xl"></FaGithub> Server</button>
+                  
+                  </Link>
+                  </div>
+                </div>
 
 
               </div>
--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -22,12 +22,12 @@ const Skills = () => {
   
   return (
     <div className=" my-40">
-      <h1 className=" text-4xl text-center border-b-2 border-gray-400 shadow-lg shadow-gray-400 font-black w-60 mx-auto">My Skills</h1>
+      <h1 className=" text-3xl lg:text-5xl text-center border-b-2 border-gray-400 shadow-lg shadow-gray-400 font-black w-60 mx-auto">My Skills</h1>
 
       <h4 className=" text-lg mt-8 lg:text-xl font-bold text-center ">Here are the technologies I work with.</h4>
 
       {isFetching&&<LoadingSpinner></LoadingSpinner>}
-      <div className=" grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 mt-10 gap-10 w-[80%] lg:w-[60%] mx-auto">{
+      <div className=" grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 mt-10 gap-10 w-[80%] lg:w-[60%] mx-auto">{
        isFetching||skills?.map(skill=><div className=" shadow-xl rounded-xl shadow-gray-400 " data-title={skill?.title} key={skill._id}>
            <Image   className=" bg-[#050505] text-white w-full h-full p-5 rounded-xl" src={skill.icon} width={80} height={80} alt={skill?.title}></Image>
         </div>)
